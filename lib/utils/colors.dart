32cc0777ae85554dcb42f23486dfa16996364d00
Riya kumari <riyakumaripat@gmail.com>
2023-08-12 19:54:18 +0530

    Update colors.dart

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -5,4 +5,4 @@ const webBackgroundColor = Color.fromRGBO(18, 18, 18, 1);
 const mobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
 const blueColor = Color.fromRGBO(0, 149, 246, 1);
 const primaryColor = Colors.white;
-const secondaryColor = Colors.grey;
+const secondaryColor = Colors.black;
